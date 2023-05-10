d4f6a476b3df76ddda9305fb095067fac627b699
emma bordres <emma.borders@menloschool.org>
2023-05-10 07:01:47 -0700

    Coded the front end

---
+++ app.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+db = SQLAlchemy(app)
+
+class Restaurant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    cuisine_style = db.Column(db.String(100), nullable=False)
+    address = db.Column(db.String(200), nullable=False)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(100), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+
+        if user is None or not user.check_password(password):
+            error = 'Invalid username or password.'
+            return render_template('login.html', error=error)
+
+        return redirect(url_for('favorites'))
+
+    return render_template('index.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if not username or not password:
+            error = 'Please enter a username and password.'
+            return render_template('signup.html', error=error)
+
+        if User.query.filter_by(username=username).first() is not None:
+            error = 'Username already exists.'
+            return render_template('signup.html', error=error)
+
+        user = User(username=username)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('home'))
+
+    return render_template('signup.html')
+
+@app.route('/favorites')
+def favorites():
+    # Get the user from the database
+    user = User.query.filter_by(username=request.form['username']).first()
+
+    # Get the user's favorite restaurants
+    favorites = Favorite.query.filter_by(user_id=user.id).all()
+
+    return render_template('favorites.html', favorites=favorites)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
